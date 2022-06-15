2b1de321e35dc2662b93530b69e1357709411595
Enrique Martín <emartinm@fdi.ucm.es>
2022-06-15 17:54:26 +0200

    Basic interpreter

---
+++ interpreter.v
@@ -0,0 +1,231 @@
+(*Require Import List.
+Import ListNotations.
+Require Import Bool.
+Require Import Arith.
+Require Import Coq.Classes.RelationClasses.
+
+Require Import Nat.
+Notation "m '=?n' n" := (m =? n) (* nat equality *)
+  (at level 100).
+
+Require Export Coq.Strings.String.
+Require Export Coq.Strings.Ascii.
+Open Scope string_scope.
+Notation "s '=?s' t" := (s =? t) (* string equality *)
+  (at level 100).*)
+  
+Require Export Coq.Strings.String.
+Require Export Coq.Strings.Ascii.
+Require Import Arith.
+Require Import Nat.
+Require Import Bool.
+Require Import bbv.Word.
+Require Import List.
+Import ListNotations.
+(*Open Scope string_scope.*)
+
+
+Module Interpreter.
+
+Definition WLen: nat := 256. 
+Definition EVMWord:= word WLen.
+Definition StackLen := 1024.
+
+(* General opcodes that operate on stack elements and return one value on top of stack*)
+Inductive gen_instr :=
+  | ADD
+  | MUL
+  | NOT.
+  
+Definition eq_gen_instr (a b: gen_instr) : bool :=
+match (a, b) with
+ | (ADD, ADD) => true
+ | (MUL, MUL) => true
+ | (NOT, NOT) => true 
+ | _ => false
+end.
+Notation "m '=?i' n" := (eq_gen_instr m n) (at level 100).
+
+(* PUSH, POP, DUP and SWAP are hardcoded, and the other opcodes are operators *)
+Inductive instr :=
+  | PUSH (size: nat) (w: EVMWord)
+  | POP 
+  | DUP (pos: nat)
+  | SWAP (pos: nat)
+  | Opcode (label: gen_instr).
+
+(* function that evaluates a list of EVMWord, they are related to gen_instr *)
+Inductive operator :=
+  | Op (comm: bool) (nb_args : nat) (func : list EVMWord -> option EVMWord).
+  
+Definition add (args: list EVMWord) : option EVMWord :=
+match args with
+ | [a; b] => Some (wplus a b)
+ | _ => None
+ end.
+ 
+Definition mul (args: list EVMWord) : option EVMWord :=
+match args with
+ | [a; b] => Some (wmult a b)
+ | _ => None
+ end.
+ 
+Definition not (args: list EVMWord) : option EVMWord :=
+match args with
+ | [a] => Some (wnot a)
+ | _ => None
+ end.
+
+
+(* Definition of maps *)
+(* The maps here link an opcode (gen_instr) to an operator *)
+
+Definition map (K V : Type) : Type := K -> option V.
+
+Definition empty_imap {A : Type} : map gen_instr A := (fun _ => None).
+Definition updatei {A : Type} (m : map gen_instr A) (x : gen_instr) (v : A) :=
+  fun x' => if x =?i x' then Some v else m x'.
+Notation "x '|->i' v ';' m" := (updatei m x v)
+  (at level 100, v at next level, right associativity).
+Notation "x '|->i' v" := (updatei empty_imap x v)
+  (at level 100).
+  
+Example opmap : map gen_instr operator :=
+ADD |->i Op true 2 add;
+MUL |->i Op true 2 mul;
+NOT |->i Op false 1 not.
+
+Definition empty_nmap {A : Type} : map nat A := (fun _ => None).
+Definition updaten {A : Type} (m : map nat A) (x : nat) (v : A) :=
+  fun x' => if x =? x' then Some v else m x'.
+Notation "x '|->n' v ';' m" := (updaten m x v)
+  (at level 100, v at next level, right associativity).
+Notation "x '|->n' v" := (updaten empty_nmap x v)
+  (at level 100).
+  
+
+Compute (
+3 |->n ADD;
+4 |->n MUL;
+5 |->n NOT).
+
+
+(* Execution states *)
+Definition tstack := list EVMWord.
+Definition tmemory := map nat EVMWord.
+Definition tstorage := map nat EVMWord.
+
+Inductive execution_state :=
+ | ExState (stack: tstack) (memory: tmemory) (storage: tstorage).
+ 
+Definition get_stack_es (es: execution_state) : tstack :=
+match es with
+| ExState stack _ _ => stack
+end.
+
+Definition set_stack_es (es: execution_state) (stack: tstack) : execution_state :=
+match es with
+| ExState _ memory storage => ExState stack memory storage
+end.
+
+Definition get_memory_es (es: execution_state) : tmemory :=
+match es with
+| ExState _ memory _ => memory
+end.
+
+Definition set_memory_es (es: execution_state) (memory: tmemory) : execution_state :=
+match es with
+| ExState stack _ storage => ExState stack memory storage
+end.
+
+Definition get_storage_es (es: execution_state) : tstorage :=
+match es with
+| ExState _ _ storage => storage
+end.
+
+Definition set_storage_es (es: execution_state) (storage: tstorage) : execution_state :=
+match es with
+| ExState stack memory _ => ExState stack memory storage
+end.
+
+(* stack manipulation operators *)
+Definition push (v : EVMWord) (es : execution_state) : option execution_state :=
+let sk := get_stack_es es in
+if List.length(sk) <? StackLen then Some (set_stack_es es (v :: sk)) else None.
+
+Definition pop (es : execution_state): option execution_state :=
+let sk := get_stack_es es in
+match sk with
+ | x::sk' => Some (set_stack_es es sk')
+ | _ => None
+end.
+
+Definition dup (k : nat) (es : execution_state) : option execution_state  :=
+let sk := get_stack_es es in
+if List.length(sk) <? StackLen then 
+  match nth_error sk k with
+  | None => None
+  | Some x => Some (set_stack_es es (x::sk))
+  end
+else None.
+
+(* THIS DOESN'T SEEM TO WORK FINE *)
+(* This operation does not changes stack size, so it cannot fail *)
+(*Definition swap__ {T : Type} (k : nat) (sk : list T) : (list T) :=
+  ((rev (firstn k sk)) ++ (skipn k sk)).
+*)
+
+
+(* MY ATTEMPT DOESN'T WORK EITHER!! *)
+Definition swap (k : nat) (es : execution_state) : option execution_state :=
+let sk := get_stack_es es in
+if ((k =? 0) || (16 <? k)) then None
+else match (nth_error sk k, sk) with
+     | (Some v, h::t) => let stack' := [v] ++ ((firstn (k-1) t)) ++ [h] ++ (skipn k sk) in
+                         Some (set_stack_es es stack') 
+     | _  => None
+     end.
+
+Compute (swap 1 (ExState [(natToWord WLen 8);(natToWord WLen 2);(natToWord WLen 3)] empty_nmap empty_nmap)).
+
+
+Definition build_es_opt_stack (es: execution_state) (h: option EVMWord) (sk: tstack) : option execution_state :=
+match h with
+| None => None
+| Some v => Some (set_stack_es es (v :: sk))
+end.
+
+
+(* Concrete interpreter *)
+Definition concr_intpreter_instr (inst : instr) (es: execution_state)
+  (ops : map gen_instr operator) : option execution_state :=
+match inst with
+  | PUSH size v => push v es
+  | POP => pop es
+  | DUP k => dup k es
+  | SWAP k => swap k es
+  | Opcode label =>
+      let insk := get_stack_es es in
+      match (ops label) with
+      | Some (Op comm nb_args func) => build_es_opt_stack es (func (firstn nb_args insk)) (skipn nb_args insk)
+      | None => None
+      end
+  end.
+
+Fixpoint concr_interpreter (insts : list instr) (es : execution_state)
+  (ops : map gen_instr operator) : option execution_state :=
+  match insts with
+  | [] => Some es
+  | inst::insts' =>
+    match (concr_intpreter_instr inst es ops) with
+    | None => None
+    | Some insk' => concr_interpreter insts' insk' ops
+    end
+  end.
+
+Compute (concr_interpreter 
+        [PUSH 1 (natToWord WLen 7); Opcode ADD] 
+        (ExState [(natToWord WLen 8);(natToWord WLen 2);(natToWord WLen 3)] empty_nmap empty_nmap)
+        opmap).
+
+End Interpreter.  
