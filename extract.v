466696c2d7e94ba57f8dae3941cf93d5c6f28925
Joseba Celaya <jcelaya@ucm.es>
2022-09-29 07:36:34 +0200

    Add ocaml naive code extraction

--- extract.v
+++ extract.v
@@ -1,54 +1,54 @@
+(** NOTES:
+
+  Some problems related to type extraction:
+   
+   - Types like bool, list and prod have straight forward representations
+   in Ocaml. nat by other hand, is extracted and represented in a naive
+   way, that might not be efficient for certain applications.
+   Also, if we extract it as an ocaml int, we would lose consistency
+   with w.r.t. to the proven theorems.
+   
+   Ways of addressing this problem are avaiable here:
+   https://softwarefoundations.cis.upenn.edu/vfa-current/Extract.html
+   
+   Currently, no fancy nat extraction might be enough.
+ 
+
+    TODO:
+
+    - Extract option
+    - Extract nat into an efficient type.
+    - Extract bbv library Word into an efficient type. *)
 
 
+(* Standar Library *)
+Require Import List.
+Import ListNotations.
 
-(* Our ModuleS *)
-Require Import Coq_EVM.datatypes. 
+(* Our Modules *)
+Require Import Coq_EVM.definitions. 
 Import EVM_Def Concrete Abstract Optimizations.
-
 Require Import Coq_EVM.interpreter.
 Import Interpreter SFS.
-
 Require Import Coq_EVM.optimizations.
 Import Optimizations. (* JOSEBA: Change module names to avoid naming collision*)
-
 Require Import Coq_EVM.checker.
 Import Checker. 
-
+Require Import Coq_EVM.examples.
+Import Examples.
 
 (* Program Extraction: OCaml as default *) 
 Import Extraction.
 
 
-(** JOSEBA:
-
-  Some problems related to type extraction:
-   Types like bool, list and prod have straight forward representations
-   in Ocaml, that are consistent by default with Coq.
-    
-   Is not the case for the type nat. This task can be tricky. This is currently
-   archieved using recommendations from:
-   https://softwarefoundations.cis.upenn.edu/vfa-current/Extract.html
-  
-
- *)
-
-
-
-
-
-(* OCaml's dafault type definitions *)
+(** Type Extractions **)
 Extract Inductive bool => "bool" [ "true" "false" ].
 Extract Inductive list => "list" [ "[]" "(::)" ].
 Extract Inductive prod => "( * )" [ "( , )" ].
-(* Coq nat default extraction is not efficient, so we should consider *)
-(* another representation. *)
 
-
-
-Recursive Extraction asfs.
-Extraction "extraction/equiv.ml" equiv_checker.
-
-
-
-
-
+(** Program Extractions **)
+(*  Examples from example.v are extracted and directly tested.
+    After running the next line, use "make run" in order to compile and
+    run the test_examples.ml file. *)
+Extraction Examples.
+Extraction "extraction/examples.ml" Examples. 
