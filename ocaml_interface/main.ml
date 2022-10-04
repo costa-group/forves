6415663e30dd8f74933ad5482d40bc6ff0120b64
Samir Genaim <genaim@Samirs-MBP.home>
2022-10-04 17:54:18 +0200

    minor changes

--- ocaml_interface/main.ml
+++ ocaml_interface/main.ml
@@ -4,22 +4,24 @@ let charlist_of_string s =
   in
   trav (String.length s) 0;;
 
-  
+
 let main () =
-      try
-        while true do
-          let p_opt = input_line stdin in (* read the optimized block *)
-            let p = input_line stdin in     (* read the original block *)
-              let k = input_line stdin in     (* read input statck size *)
-                (* call the checker -- convering strings to corresponding lists of chars *)
-                let r = Checker.Parser.block_eq (charlist_of_string p_opt) (charlist_of_string p) (charlist_of_string k) in
-                  (* print the result *)
-                  match r with
-                    | None -> print_endline "--"; print_endline "Something went wrong while checking the following instance:"; print_endline ""; print_endline p_opt; print_endline p; print_endline k; print_endline "--";
-                    | Some b -> print_endline (string_of_bool b)
-        done              
-     with
-        | End_of_file -> ();
-        | e -> raise e
-                                               
-let _ = main ()
+  let i = ref 0 in
+  try
+    while true do
+      i := !i+1;
+      let p_opt = input_line stdin in (* read the optimized block *)
+      let p = input_line stdin in     (* read the original block *)
+      let k = input_line stdin in     (* read input statck size *)
+      (* call the checker -- convering strings to corresponding lists of chars *)
+      let r = Checker.Parser.block_eq (charlist_of_string p_opt) (charlist_of_string p) (charlist_of_string k) in
+      (* print the result *)
+      match r with
+      | None -> Printf.printf "Eaxmple %d = parsing error\n\n  %s\n  %s\n  %s\n\n" !i p_opt p k;
+      | Some b -> Printf.printf "Eaxmple %d = %B\n" !i b;
+    done
+  with
+  | End_of_file -> ();
+  | e -> raise e;;
+
+let _ = main ();;
