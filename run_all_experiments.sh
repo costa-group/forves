3cdfa24a143aeee5c6a2307680d33322101c1373
Enrique Martín <emartinm@ucm.es>
2023-01-20 11:57:11 +0100

    Improved benchmarks

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,34 +1,31 @@
 #! /bin/bash
 
+TMPFILE=/tmp/forves_out
+
 for f in ./blocks/*.txt;  do
-    nlines=`wc -l < $f`
-    nblocks=$(( nlines/3 ))
 	echo "$f"
-	echo "($nblocks blocks)";
-	echo '-----------------------------------------------------'
-	echo '| Checker                      |  #Yes   | Time (s) |'
-	echo '-------------------------------|---------|----------|'
+	
 	start=`date +%s%N`
-	n0=`./bin/checker -alg 0 < $f | grep true | wc -l`
+	./bin/checker -alg 0 < $f > $TMPFILE
 	end=`date +%s%N`
+	nblocks=`wc -l < $TMPFILE`
+	n0=`cat $TMPFILE | grep true | wc -l`
 	ex_time_ns=`expr $end - $start`
 	ex_time0_s=`echo "scale=2; $ex_time_ns / 1000000000" | bc`
  
     start=`date +%s%N`
-	n1=`./bin/checker -alg 1 < $f | grep true | wc -l`
+	./bin/checker -alg 2 < $f > $TMPFILE
 	end=`date +%s%N`
-	ex_time_ns=`expr $end - $start`
-	ex_time1_s=`echo "scale=2; $ex_time_ns / 1000000000" | bc`
-	
-	start=`date +%s%N`
-	n2=`./bin/checker -alg 2 < $f | grep true | wc -l`
-	end=`date +%s%N`
+	n2=`cat $TMPFILE | grep true | wc -l`
 	ex_time_ns=`expr $end - $start`
 	ex_time2_s=`echo "scale=2; $ex_time_ns / 1000000000" | bc`
-	
-	echo "| evm_eq_block_chkr   (CHKR)   |     $n0 |      $ex_time0_s |"
-    echo "| evm_eq_block_chkr'  (CHKR')  |     $n1 |      $ex_time1_s |"
-    echo "| evm_eq_block_chkr'' (CHKR'') |     $n2 |      $ex_time2_s |"
-    echo '-----------------------------------------------------'
+
+    echo "($nblocks blocks)";
+	echo '--------------------------------'
+	echo '| Checker |  #Yes   | Time (s) |'
+	echo '|---------|---------|----------|'	
+	printf "| CHKR    | %7s | %8s |\n" $n0 $ex_time0_s
+    printf "| CHKR^o  | %7s | %8s |\n" $n2 $ex_time2_s
+    echo '--------------------------------'
 	echo
 done;
