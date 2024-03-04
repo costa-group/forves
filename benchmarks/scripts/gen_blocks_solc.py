438679234e022c47926abd77e5c824f1b8c56f75
Enrique Martín <emartinm@ucm.es>
2024-03-04 19:05:54 +0100

    Scripts

--- benchmarks/scripts/gen_blocks_solc.py
+++ benchmarks/scripts/gen_blocks_solc.py
@@ -1,59 +1,130 @@
+import gzip
 import os
-import sys
-import subprocess
 import shutil
+import subprocess
+import sys
 
-from gen_tests_for_ocaml import gen_blocks_from_daniel_format
-
+from gen_tests_for_ocaml import gen_blocks_from_daniel_format_gz, gen_blocks_from_daniel_format
 
 # solc compiler that generates pre and post optimizations bytecode
 # https://github.com/ethereum/solidity/compare/develop...forves
 SOLC_PATH = '/tmp/solc'
 
 
-def gen_evm_json(dir):
+def copy_gz_json(dest_gz):
+    # Copy the file /tmp/blockTrace.json as the GZ file 'dest_gz'
+    with open('/tmp/blockTrace.json', 'rb') as json_file:
+        content = json_file.read()
+        with gzip.open(dest_gz, 'wb') as f:
+            f.write(content)
+    print(f'Generated {dest_gz}')
+
+
+def gen_evm_json_gz(dir):
     i = 0
-    for dirpath,_,filenames in os.walk(dir):
+    for dirpath, _, filenames in os.walk(dir):
         dirname = os.path.basename(dirpath)
         if dirname.startswith('0x') and f"{dirname}.sol" in filenames:
             i += 1
-            print(f"Processing {dirpath}")
-            #ret = os.system(f'~/solc {dirname}.sol')
-            proc = subprocess.run([SOLC_PATH, '--bin', '--optimize', f'{dirname}.sol'], shell=False, capture_output=True, cwd=dirpath)
+            print(f"Compiling {dirpath}")
+            proc = subprocess.run([SOLC_PATH, '--bin', '--optimize', '--optimize-runs', '4294967295',
+                                   f'{dirname}.sol'], shell=False, capture_output=True, cwd=dirpath)
             print(proc.returncode)
             if proc.returncode == 0:
-                dest = os.path.join(dirpath, 'blockTrace.json')
-                dest_check = shutil.copyfile('/tmp/blockTrace.json', dest)
-                print(f'Generated  {dest_check}')
+                dest_gz = os.path.join(dirpath, 'blockTrace.json.gz')
+                copy_gz_json(dest_gz)
+                # dest = os.path.join(dirpath, 'blockTrace.json')
+                # dest_check = shutil.copyfile('/tmp/blockTrace.json', dest)
+                # print(f'Generated  {dest_check}')
 
-            proc = subprocess.run([SOLC_PATH, '--bin', '--via-ir', '--optimize', f'{dirname}.sol'], shell=False, capture_output=True, cwd=dirpath)
+            proc = subprocess.run([SOLC_PATH, '--bin', '--via-ir', '--optimize', '--optimize-runs', '4294967295',
+                                   f'{dirname}.sol'], shell=False,
+                                  capture_output=True, cwd=dirpath)
             print(proc.returncode)
             if proc.returncode == 0:
-                dest = os.path.join(dirpath, 'blockTrace_yul.json')
-                dest_check = shutil.copyfile('/tmp/blockTrace.json', dest)
-                print(f'Generated  {dest_check}')
-            print('\n\n\n')
+                dest_gz = os.path.join(dirpath, 'blockTrace_yul.json.gz')
+                copy_gz_json(dest_gz)
+                # dest = os.path.join(dirpath, 'blockTrace_yul.json')
+                # dest_check = shutil.copyfile('/tmp/blockTrace.json', dest)
+                # print(f'Generated  {dest_check}')
+            print('\n\n')
     print(i)
 
 
-def gen_blocks(dir):
-    for dirpath,_,filenames in os.walk(dir):
+def gen_blocks_from_json_gz(directory):
+    for dirpath, _, filenames in os.walk(directory):
+        smart_contract = os.path.basename(dirpath)
+        if 'blockTrace.json.gz' in filenames:
+            json_path = os.path.join(dirpath, 'blockTrace.json.gz')
+            blocks_path = os.path.join(dirpath, 'blocks.txt')
+            print(f'Extracting blocks from {json_path}', flush=True)
+            blocks = gen_blocks_from_daniel_format_gz(json_path, smart_contract)
+            with open(blocks_path, 'wt', encoding='utf8') as f:
+                f.write(blocks)
+        if 'blockTrace_yul.json.gz' in filenames:
+            json_path = os.path.join(dirpath, 'blockTrace_yul.json.gz')
+            blocks_path = os.path.join(dirpath, 'blocks_yul.txt')
+            print(f'Extracting blocks from {json_path}', flush=True)
+            blocks = gen_blocks_from_daniel_format_gz(json_path, smart_contract)
+            with open(blocks_path, 'wt', encoding='utf8') as f:
+                f.write(blocks)
+
+
+def gen_blocks_from_json(dir):
+    for dirpath, _, filenames in os.walk(dir):
+        smart_contract = os.path.basename(dirpath)
         if 'blockTrace.json' in filenames:
             json_path = os.path.join(dirpath, 'blockTrace.json')
             blocks_path = os.path.join(dirpath, 'blocks.txt')
             print(f'Processing {json_path}', flush=True)
-            blocks = gen_blocks_from_daniel_format(json_path)
+            blocks = gen_blocks_from_daniel_format(json_path, smart_contract)
             with open(blocks_path, 'w', encoding='utf8') as f:
                 f.write(blocks)
         if 'blockTrace_yul.json' in filenames:
             json_path = os.path.join(dirpath, 'blockTrace_yul.json')
             blocks_path = os.path.join(dirpath, 'blocks_yul.txt')
             print(f'Processing {json_path}', flush=True)
-            blocks = gen_blocks_from_daniel_format(json_path)
+            blocks = gen_blocks_from_daniel_format(json_path, smart_contract)
             with open(blocks_path, 'w', encoding='utf8') as f:
                 f.write(blocks)
-    
+
+
+def gen_blocks_no_json(dir):
+    json_path = '/tmp/blockTrace.json'
+    i = 0
+    for dirpath, _, filenames in os.walk(dir):
+        dirname = os.path.basename(dirpath)
+        smart_contract = os.path.basename(dirpath)
+        if dirname.startswith('0x') and f"{dirname}.sol" in filenames:
+            i += 1
+            print(f"Compiling {dirpath}")
+            proc = subprocess.run([SOLC_PATH, '--bin', '--optimize', '--optimize-runs', '4294967295',
+                                   f'{dirname}.sol'], shell=False, capture_output=True, cwd=dirpath)
+            print(proc.returncode)
+            if proc.returncode == 0:
+                blocks_path = os.path.join(dirpath, 'blocks.txt')
+                print(f'Processing {json_path}', flush=True)
+                blocks = gen_blocks_from_daniel_format(json_path, smart_contract)
+                with open(blocks_path, 'wt', encoding='utf8') as f:
+                    f.write(blocks)
+                    f.close()
+
+            proc = subprocess.run([SOLC_PATH, '--bin', '--via-ir', '--optimize', '--optimize-runs', '4294967295',
+                                   f'{dirname}.sol'], shell=False,
+                                  capture_output=True, cwd=dirpath)
+            print(proc.returncode)
+            if proc.returncode == 0:
+                blocks_path = os.path.join(dirpath, 'blocks_yul.txt')
+                print(f'Processing {json_path}', flush=True)
+                blocks = gen_blocks_from_daniel_format(json_path, smart_contract)
+                with open(blocks_path, 'wt', encoding='utf8') as f:
+                    f.write(blocks)
+                    f.close()
+            print('\n\n')
+    print(i)
+
 
 if __name__ == "__main__":
-    # gen_evm_json(sys.argv[1])
-    gen_blocks(sys.argv[1])
+    # gen_evm_json_gz(sys.argv[1])
+    # gen_blocks_from_json_gz(sys.argv[1])
+    gen_blocks_no_json(sys.argv[1])
--- benchmarks/scripts/gen_tests_for_ocaml.py
+++ benchmarks/scripts/gen_tests_for_ocaml.py
@@ -1,28 +1,39 @@
-import re
-from typing import List
-import sys
-import os
 import csv
+import gzip
 import json
+import os
+import re
+import sys
+from typing import List
 
 include_identical = False
 
 #
 #
-bytecode_vocab = [ 'ADD', 'MUL', 'NOT', 'SUB', 'DIV', 'SDIV', 'MOD', 'SMOD', 'ADDMOD', 'MULMOD', 'EXP', 'SIGNEXTEND', 'LT', 'GT', 'SLT', 'SGT', 'EQ', 'ISZERO', 'AND', 'OR', 'XOR', 'BYTE', 'SHL', 'SHR', 'SAR', 'SHA3', 'KECCAK256', 'ADDRESS', 'BALANCE', 'ORIGIN', 'CALLER', 'CALLVALUE', 'CALLDATALOAD', 'CALLDATASIZE ', 'CODESIZE', 'GASPRICE', 'EXTCODESIZE', 'RETURNDATASIZE', 'EXTCODEHASH', 'BLOCKHASH', 'COINBASE', 'TIMESTAMP', 'NUMBER', 'DIFFICULTY', 'GASLIMIT', 'CHAINID', 'SELFBALANCE', 'BASEFEE', 'SLOAD', 'MLOAD', 'MSTORE', 'MSTORE8', 'SSTORE', 'PC', 'MSIZE', 'GAS', 'CREATE', 'CREATE2', 'CALLDATASIZE', 'CALLDATALOAD', 'JUMPI', 'JUMPDEST', 'METAPUSH',
-    'POP',
-    'DUP1', 'DUP2', 'DUP3', 'DUP4', 'DUP5', 'DUP6', 'DUP7', 'DUP8', 'DUP9', 'DUP10', 'DUP11', 'DUP12', 'DUP13', 'DUP14', 'DUP15', 'DUP16',
-    'SWAP1', 'SWAP2', 'SWAP3', 'SWAP4', 'SWAP5', 'SWAP6', 'SWAP7', 'SWAP8', 'SWAP9', 'SWAP10', 'SWAP11', 'SWAP12', 'SWAP13', 'SWAP14', 'SWAP15', 'SWAP16',
+bytecode_vocab = ['ADD', 'MUL', 'NOT', 'SUB', 'DIV', 'SDIV', 'MOD', 'SMOD', 'ADDMOD', 'MULMOD', 'EXP', 'SIGNEXTEND',
+                  'LT', 'GT', 'SLT', 'SGT', 'EQ', 'ISZERO', 'AND', 'OR', 'XOR', 'BYTE', 'SHL', 'SHR', 'SAR', 'SHA3',
+                  'KECCAK256', 'ADDRESS', 'BALANCE', 'ORIGIN', 'CALLER', 'CALLVALUE', 'CALLDATALOAD', 'CALLDATASIZE ',
+                  'CODESIZE', 'GASPRICE', 'EXTCODESIZE', 'RETURNDATASIZE', 'EXTCODEHASH', 'BLOCKHASH', 'COINBASE',
+                  'TIMESTAMP', 'NUMBER', 'DIFFICULTY', 'GASLIMIT', 'CHAINID', 'SELFBALANCE', 'BASEFEE', 'SLOAD',
+                  'MLOAD', 'MSTORE', 'MSTORE8', 'SSTORE', 'PC', 'MSIZE', 'GAS', 'CREATE', 'CREATE2', 'CALLDATASIZE',
+                  'CALLDATALOAD', 'JUMPI', 'JUMPDEST', 'METAPUSH',
+                  'POP',
+                  'DUP1', 'DUP2', 'DUP3', 'DUP4', 'DUP5', 'DUP6', 'DUP7', 'DUP8', 'DUP9', 'DUP10', 'DUP11', 'DUP12',
+                  'DUP13', 'DUP14', 'DUP15', 'DUP16',
+                  'SWAP1', 'SWAP2', 'SWAP3', 'SWAP4', 'SWAP5', 'SWAP6', 'SWAP7', 'SWAP8', 'SWAP9', 'SWAP10', 'SWAP11',
+                  'SWAP12', 'SWAP13', 'SWAP14', 'SWAP15', 'SWAP16',
                   ]
+
 
 #
 #
 def is_pseudo_keyword(opcode: str) -> bool:
-    if opcode.find("tag") ==-1 and opcode.find("#") ==-1 and opcode.find("$") ==-1 \
-            and opcode.find("data") ==-1:
+    if opcode.find("tag") == -1 and opcode.find("#") == -1 and opcode.find("$") == -1 \
+            and opcode.find("data") == -1:
         return False
     else:
         return True
+
 
 def keyword_to_id(keyword):
     if keyword == "PUSHDEPLOYADDRESS":
@@ -43,7 +54,6 @@ def keyword_to_id(keyword):
         return 7
     else:
         raise Exception(f'uknown meta push keyword: {keyword}')
-       
 
 
 def split_bytecode(raw_instruction_str: str) -> List[str]:
@@ -56,8 +66,8 @@ def split_bytecode(raw_instruction_str: str) -> List[str]:
         op = ops[i]
 
         # JUMPDEST is removed from the block - later maybe we should support
-        if op.startswith("JUMPDEST"):  
-           i += 1
+        if op.startswith("JUMPDEST"):
+            i += 1
 
         # In theory, they should not appear inside a block, as they are removed beforehand.
         # Nevertheless, we include them just in case
@@ -66,7 +76,7 @@ def split_bytecode(raw_instruction_str: str) -> List[str]:
             i += 1
 
         # if it does not start with PUSH, then its an opcode with no operands
-        elif not op.startswith("PUSH"): 
+        elif not op.startswith("PUSH"):
             final_op = op
 
         # op starts with PUSH
@@ -74,7 +84,7 @@ def split_bytecode(raw_instruction_str: str) -> List[str]:
             # Just in case PUSHx instructions are included, we translate them to "PUSH x" name instead
             if re.fullmatch("PUSH([0-9]+)", op) is not None:
                 final_op = op
-                operand = f'0x{ops[i+1]}'
+                operand = f'0x{ops[i + 1]}'
                 i = i + 1
             elif op == "PUSHDEPLOYADDRESS" or op == "PUSHSIZE":
                 final_op = "METAPUSH"
@@ -83,46 +93,44 @@ def split_bytecode(raw_instruction_str: str) -> List[str]:
             elif op == "PUSHLIB" or op == "PUSHIMMUTABLE":
                 final_op = "METAPUSH"
                 meta_operand = f'{keyword_to_id(op)}'
-                operand = f'0x{ops[i+1]}'
+                operand = f'0x{ops[i + 1]}'
                 i = i + 1
-#            elif op == "PUSH" and ops[i+1] == "[tag]":  # splitted fom the next case due to a bug in PUSH [tag]
-#                final_op = "METAPUSH"
-#                meta_operand = f'{keyword_to_id(ops[i+1])}'
-#                operand = f'0x0'
-#                i = i + 2
-            elif op == "PUSH" and is_pseudo_keyword(ops[i+1]):
+            #            elif op == "PUSH" and ops[i+1] == "[tag]":  # splitted fom the next case due to a bug in PUSH [tag]
+            #                final_op = "METAPUSH"
+            #                meta_operand = f'{keyword_to_id(ops[i+1])}'
+            #                operand = f'0x0'
+            #                i = i + 2
+            elif op == "PUSH" and is_pseudo_keyword(ops[i + 1]):
                 final_op = "METAPUSH"
-                meta_operand = f'{keyword_to_id(ops[i+1])}'
-                operand = f'0x{ops[i+2]}'
+                meta_operand = f'{keyword_to_id(ops[i + 1])}'
+                operand = f'0x{ops[i + 2]}'
                 i = i + 2
             # A  PUSH 
             elif op == "PUSH":
-                n =  (len(ops[i+1])+1) // 2
+                n = (len(ops[i + 1]) + 1) // 2
                 assert n >= 1 and n <= 32
                 final_op = f'PUSH{n}'
-                operand = f'0x{ops[i+1]}'
+                operand = f'0x{ops[i + 1]}'
                 i = i + 1
 
             # Something that we don't handle, will just leave it and an exception will be thrown    
             else:
                 raise Exception("...")
-            
+
         opcodes.append(final_op)
         if meta_operand is not None:
             opcodes.append(meta_operand)
-            meta_operand=None
+            meta_operand = None
         if operand is not None:
             opcodes.append(operand)
-            operand=None
+            operand = None
 
         i += 1
 
     return opcodes
 
 
-
-
-def bin_to_word(b : str) :
+def bin_to_word(b: str):
     word = 'WO'
     for d in b:
         if d == '0':
@@ -131,45 +139,48 @@ def bin_to_word(b : str) :
             word = f'(WS true {word})'
     return word
 
-def encode_num(n_hex : str):
+
+def encode_num(n_hex: str):
     return f'(NToWord WLen {n_hex}%N)'
 
+
 def str_to_list(bytecode_str):
-    bytecode_seq =  split_bytecode(bytecode_str)
+    bytecode_seq = split_bytecode(bytecode_str)
     n = len(bytecode_seq)
     if n == 0:
         raise Exception("zero length")
-    
-    out_seq=[]
+
+    out_seq = []
     i = 0;
-    while i<n:
+    while i < n:
         instr = bytecode_seq[i]
         if re.fullmatch("PUSH([0-9]+)", instr) is not None:
-            out_seq.append( bytecode_seq[i])
-            out_seq.append( bytecode_seq[i+1])
+            out_seq.append(bytecode_seq[i])
+            out_seq.append(bytecode_seq[i + 1])
             i = i + 2
         elif instr == "METAPUSH":
-            out_seq.append( bytecode_seq[i])
-            out_seq.append( bytecode_seq[i+1])
-            out_seq.append( bytecode_seq[i+2])
+            out_seq.append(bytecode_seq[i])
+            out_seq.append(bytecode_seq[i + 1])
+            out_seq.append(bytecode_seq[i + 2])
             i = i + 3
         else:
-            idx = bytecode_vocab.index(instr) # just check the instruction is supported
+            idx = bytecode_vocab.index(instr)  # just check the instruction is supported
             out_seq.append(instr)
             i = i + 1
 
     return out_seq
 
+
 #
 #
-def print_test(bench_id,block_info, block_sfs):
+def print_test(bench_id, block_info, block_sfs):
     try:
-        if not block_info["model_found"]=="True":
+        if not block_info["model_found"] == "True":
             return
 
         if not include_identical and block_info["previous_solution"].lower() == block_info["solution_found"].lower():
             return
-        
+
         bytecode_as_list = str_to_list(block_info["previous_solution"])
         opt_bytecode_as_list = str_to_list(block_info["solution_found"])
 
@@ -185,10 +196,11 @@ def print_test(bench_id,block_info, block_sfs):
         print(bytecode)
         print(stack_size)
         print()
-        return (len(bytecode_as_list),len(opt_bytecode_as_list))
+        return (len(bytecode_as_list), len(opt_bytecode_as_list))
     except Exception as e:
-        print(e,file=sys.stderr)
+        print(e, file=sys.stderr)
         return None
+
 
 #
 #
@@ -214,8 +226,7 @@ def gen_tests(paths):
                             total_p = total_p + r[0]
                             total_opt_p = total_p + r[1]
                             i = i + 1
-    print(f'{total_p/i:.2f},{total_opt_p/i:.2f}',file=sys.stderr)
-
+    print(f'{total_p / i:.2f},{total_opt_p / i:.2f}', file=sys.stderr)
 
 
 def solc_json_block_to_str(b):
@@ -229,8 +240,9 @@ def solc_json_block_to_str(b):
             s += f'{o["name"]}{v} '
     return s[:-1]
 
+
 def print_test_2(bstr1, bstr2):
-    try:     
+    try:
         bytecode_as_list = str_to_list(bstr1)
         opt_bytecode_as_list = str_to_list(bstr2)
 
@@ -244,11 +256,11 @@ def print_test_2(bstr1, bstr2):
         print(stack_size)
         print()
     except Exception as e:
-        print(f'>>>> {e}',file=sys.stderr)
+        print(f'>>>> {e}', file=sys.stderr)
 
 
 def gen_test_2(bstr1, bstr2, i, contract=""):
-    try:     
+    try:
         bytecode_as_list = str_to_list(bstr1)
         opt_bytecode_as_list = str_to_list(bstr2)
         if bytecode_as_list == opt_bytecode_as_list:
@@ -261,38 +273,59 @@ def gen_test_2(bstr1, bstr2, i, contract=""):
         return f'# Smart contract {contract}\n# Block {i}\n{opt_bytecode}\n{bytecode}\n{stack_size}\n\n'
     except Exception as e:
         # print(f'>>>> {e}',file=sys.stderr)
-        return ""       
+        return ""
+
 
 def gen_tests_from_daniel_format(paths):
     for path in paths:
         with open(path, 'r', encoding='utf8') as f:
             json_source = f.read()
             bs = json.loads(f'[{json_source[:-2]}]')  # Removes last comma
-            for i,b in enumerate(bs):
-                print(f'>>>> Bloque {i}',file=sys.stderr)
+            for i, b in enumerate(bs):
+                print(f'>>>> Bloque {i}', file=sys.stderr)
                 b1 = solc_json_block_to_str(b["pre"][".code"])
                 b2 = solc_json_block_to_str(b["post"][".code"])
-                print_test_2(b1,b2)
+                print_test_2(b1, b2)
 
-def gen_blocks_from_daniel_format(path):
+
+def gen_blocks_from_daniel_format(path, smart_contract_name=""):
+    # Removes duplicates in the same smart contract
+    past = set()
     with open(path, 'r', encoding='utf8') as f:
         json_source = f.read()
         bs = json.loads(f'[{json_source[:-2]}]')  # Removes last comma
         blocks = ""
-        for i,b in enumerate(bs):
+        for i, b in enumerate(bs):
             # print(f'>>>> Bloque {i}',file=sys.stderr)
             b1 = solc_json_block_to_str(b["pre"][".code"])
             b2 = solc_json_block_to_str(b["post"][".code"])
-            blocks += gen_test_2(b1,b2,i,path)
+            if (b1, b2) not in past:
+                blocks += gen_test_2(b1, b2, i, smart_contract_name)
+                past.add((b1, b2))
+
         return blocks
+
+
+def gen_blocks_from_daniel_format_gz(path, smart_contract_name=""):
+    with gzip.open(path, 'rb') as f:
+        json_source = f.read().decode('utf-8')
+        bs = json.loads(f'[{json_source[:-2]}]')  # Removes last comma
+        blocks = ""
+        for i, b in enumerate(bs):
+            # print(f'>>>> Bloque {i}',file=sys.stderr)
+            b1 = solc_json_block_to_str(b["pre"][".code"])
+            b2 = solc_json_block_to_str(b["post"][".code"])
+            blocks += gen_test_2(b1, b2, i, smart_contract_name)
+        return blocks
+
 
 # Usage example:
 #
 #   python3 gen_tests.py /path-to/results_coq_evm/no_rl_gas_opt > no_rl_gas_opt_tests.v
 #
 if __name__ == "__main__":
-    paths=sys.argv[1:]
-    #gen_tests(paths)
+    paths = sys.argv[1:]
+    # gen_tests(paths)
     gen_tests_from_daniel_format(paths)
-    #x = str_to_list("PUSH [tag] 1 ADD SUB PUSH 10")
-    #print(x)
+    # x = str_to_list("PUSH [tag] 1 ADD SUB PUSH 10")
+    # print(x)
